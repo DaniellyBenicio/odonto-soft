37caaf573d64a3477f866b5ab0ed87ead6dced18
Danielly Benicio <daniellybeniciodearaujo@gmail.com>
2025-07-12 11:55:07 -0300

    config client

---
+++ server/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+//import authRoutes from "./routes/authRoutes.js";
+//import userRoutes from "./routes/admin/userRoutes.js";
+
+const app = express();
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+
+//app.use("/api/auth", authRoutes);
+//app.use("/api", userRoutes);
+
+app.listen(3000, () => {
+  console.log("API rodando na porta 3000");
+});
